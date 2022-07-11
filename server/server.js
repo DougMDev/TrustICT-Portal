a4329a7a461f6e27f8e347b0562845a6c95df472
unknown <DougMDev@outlook.com>
2022-07-11 15:21:26 +0100

    Basic routing tests setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const testRoutes = require("./Routes/test");
 const port = 3001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend Online");
+app.use("/api", testRoutes);
+
+app.get("*", (req, res) => {
+  res.send("We're Sorry, that page cannot be found..");
 });
 
 app.listen(port, () => {
